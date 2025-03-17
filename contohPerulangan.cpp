7f78079dd6deca64bbf1f45b64a7c4f90cf9964a
Jeremyadrean6 <bangje43121@gmail.com>
2025-03-17 15:30:25 +0700

    menambahakan increment pencacah turun

--- contohPerulangan.cpp
+++ contohPerulangan.cpp
@@ -19,4 +19,12 @@ int main()
     cout << "nilai n terakhir = " << n << endl;
     cout << endl;
 
+    cout << "perulangan for pencacah turun " << endl;
+    // perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "nilai n = " << n << " selamat datang " << endl;
+    }
+    cout << nilai n terakhir = << n;
+    cout << endl;
 } 
