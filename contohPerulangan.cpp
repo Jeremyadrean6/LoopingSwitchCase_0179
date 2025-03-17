2abfc855476f394f229f3dc178e1ab055fe9837c
Jeremyadrean6 <bangje43121@gmail.com>
2025-03-17 15:25:35 +0700

    menambahakan increment pencacah naik

--- contohPerulangan.cpp
+++ contohPerulangan.cpp
@@ -9,4 +9,14 @@ int main()
     int perulanganwhile = 0;
     int perulangan = 0;
 
+    //perulangan dengan for
+
+    cout << "perulangan for pencecah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "nilai n = " << n << " selamat datang " << endl
+    }
+    cout << "nilai n terakhir = " << n << endl;
+    cout << endl;
+
 } 
